ed249b41da816621d91ca5c74614eddbf21127d4
starkhu <starkhya@163.com>
2018-08-18 19:37:46 +0800

    size test

---
+++ base_data_structure/size_function.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+void size_test() {
+	struct ListNode {
+		//char value_c;
+		int value_i;
+		char value_c0;
+		char value_c1;
+		//ListNode* pNext;
+	};
+	ListNode* pNode = new ListNode;
+	//指针占4个字节 
+	std::cout << "size of ListNode is: " << sizeof(ListNode) << std::endl;
+	std::cout << "size of int is: " << sizeof(char) << std::endl;
+	std::cout << "size of int* is: " << sizeof(int*) << std::endl;
+}
