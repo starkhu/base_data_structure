0579c70e8d2c7378eb56574b2c43300b56aea4c1
starkhu <starkhya@163.com>
2018-08-18 19:37:20 +0800

    char test

---
+++ base_data_structure/char.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+void char_test() {
+	char str1[] = "hello world";
+	char str2[] = "hello world";
+	char* str3 = "hello world";
+	char* str4 = "hello world";
+	std::cout << str1 << std::endl;
+	std::cout << str2 << std::endl;
+	std::cout << str3 << std::endl;
+	std::cout << str4 << std::endl;
+	if (str1 == str2)
+		std::cout << "str1 and str2 are same" << std::endl;
+	else
+		std::cout << "str1 and str2 are different" << std::endl;
+	if (str3 == str4)
+		std::cout << "str3 and str4 are same" << std::endl;
+	else
+		std::cout << "str3 and str4 are different" << std::endl;
+}
