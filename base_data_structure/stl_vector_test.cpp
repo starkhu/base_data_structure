d98ea963e50d9f807a75a5834d264e1c66d7ce84
starkhu <starkhya@163.com>
2018-09-10 11:10:48 +0800

    add notation

--- base_data_structure/stl_vector_test.cpp
+++ base_data_structure/stl_vector_test.cpp
@@ -1,29 +1,35 @@
 #include<iostream>
 #include<vector>
 
-void VectorTest() {
+void Vector1DTest() {
 	//1D array
 	std::vector<int> vec, v1, v2, v3;
 	std::cout << "maxsize of is" << vec.max_size() << std::endl;
+	//size表示vec中拥有的元素的个数
+	//capacity表示系统给vec分配的内存空间的大小
 	for (int i = 0; i < 10; i++) {
 		vec.push_back(2 * i);
 		std::cout << "size of vec is " << vec.size() << std::endl;
 		std::cout << "capacity of vec is " << vec.capacity() << std::endl;
 	}
 
-	//resize
+	//resize 
 	//如果resize（n）中n小于当前vector中元素个数，则舍弃后面的元素
+	//如果resize(n)中n大于当前vector中的元素个数，则分配n个内存空间，并初始化后面的内存空间
 	vec.resize(8);
 	std::cout << "size of vec is " << vec.size() << std::endl;
 	vec.resize(20);
 	std::cout << "size of vec is " << vec.size() << std::endl;
+	std::cout << "capacity of vec is " << vec.capacity() << std::endl;
 
 	//reserve
 	//如果reserve(n)中n小于当前vector中元素个数，则调用直接返回
-	vec.reserve(8);
+	//如果reserve(n)中n大于当前vector中元素个数，只分配n个内存空间，不会初始化新分配的内存空间
+	vec.reserve(6);
 	std::cout << "size of vec is " << vec.size() << std::endl;
 	vec.reserve(24);
 	std::cout << "size of vec is " << vec.size() << std::endl;
+	std::cout << "capacity of vec is " << vec.capacity() << std::endl;
 
 	//去掉最后一个元素
 	vec.pop_back();
@@ -52,7 +58,16 @@ void VectorTest() {
 
 	//查找
 	std::cout << "vec.at(3) is " << vec.at(3) << std::endl;
+	//std::cout << "vec.at(17) is " << vec.at(17) << std::endl;
+	//::cout << "vec[17] is " << vec[17] << std::endl;
 	
+	//assign
+	//v0的空间是vec的size, 不是vec的capicity.
+	std::vector<int> v0(vec);
+	std::cout << "capacity of vec is " << vec.capacity() << std::endl;
+	std::cout << "size of vec is " << vec.size() << std::endl;
+	std::cout << "capacity of v0 is " << v0.capacity() << std::endl;
+
 	//assign
 	v1 = vec;
 	std::vector<int>::iterator iter2;
@@ -76,4 +91,8 @@ void VectorTest() {
 		std::cout << *iter2 << " ";
 	std::cout << std::endl;
 	std::swap(v1, v2);
+}
+
+void VectorTest() {
+	Vector1DTest();
 }
